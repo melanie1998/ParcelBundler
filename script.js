6e3f52c9d7e30b57c5d96a3698b33affe7035d6c
2dam3 <2dam3@MININT-EPL6B0M.H012108.NET>
2020-11-20 13:27:52 +0100

    actualizado

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-[a, b] = [40, 60];
-const rest = [1,2,3,4]; 
-const other = [a, b,...rest]
+import L from 'leaflet';
+import gsap from 'gsap';
 
-console.log(other);
+gsap.to('h2', {duration: 5, x:770});
+gsap.to('#map', {duration: 5, x:-1820, opacity: 1});
+// Create the map
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
